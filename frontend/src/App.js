e2f5245d13a79a8a380edbce03dd6c32cb0627a1
kgandham <kevin_gandham@sfu.ca>
2022-01-17 22:03:00 -0800

    webpage displays

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
+import Login from "./views/Login";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
